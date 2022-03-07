9272bbe624773931ab81b58406ae521a45db21f9
jmaraya1229 <jessemaraya2013@gmail.com>
2022-03-06 19:56:19 -0800

    added view code

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const connection = require ("./config/connection");
 const inquirer = require("inquirer");
 const conTable = require("console.table");
 
-connection.connect((error) => {
-    if (error) throw error;
+connection.connect((err) => {
+    if (err) throw err;
     promptQ(); 
 });
 
@@ -76,6 +76,95 @@ const promptQ = () => {
         }
     })
 };
+
+// -------------- View All Employees --------------
+const viewAllEmployees = () => {
+    let sql = 
+        `SELECT employee.id,
+        employee.first_name, 
+        employee.last_name, 
+        role.title, 
+        department.department_name AS "department",
+        role.salary
+        FROM employee, role, department
+        WHERE department.id = role.depart_id
+        AND role.id = employee.role_id
+        ORDER BY employee.id ASC
+        `;
+
+    connection.promise().query(sql, (err, response) => {
+        if (err) throw err;
+        promptQ();
+    })
+};
+
+// --------------Add Employee --------------
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: input,
+            name: "firstName",
+            message: "What is the first name of the employee?",
+        },
+        {
+            type: input,
+            name: "lastName",
+            message: "What is the last name of the employee?",
+        }
+    ])
+    .then(answer => {
+        const employeeName = [answer.firstName, answer.lastName]
+        const roleSQL = `SELECT role.id, role.title FROM role`;
+        connection.promise().query(roleSQL, (err, data) => {
+            if (err) throw err;
+            const roles = data.map(({ id, title}) => ({ name: title, value: id}));
+            inquirer.prompt([
+                {
+                    type: "list",
+                    name: "role",
+                    message: "What is the employee's role?",
+                    choices: roles
+                }
+            ])
+            .then (roleChoice => {
+                let role = roleChoice.role;
+                employeeName.push(role);
+                let managerSQL = `SELECT * FROM employee`;
+                connection.promise().query(managerSQL, (err, data) => {
+                    if (err) throw err;
+                    let managers = data.map (({ id, first_name, last_name }) => ({name: first_name + " " + last_name, value: id }));
+                    inquirer.prompt([
+                        {
+                            type: "list",
+                            name: "manager",
+                            message: "Who is the employee's manager?",
+                            choices: managers
+                        }
+                ])
+                    .then(managerChoice => {
+                        let manager = managerChoice.manager;
+                        employeeName.push(manager);
+                        let sql = 
+                        `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                        VALUES (?, ?, ?, ?)`;
+                        connection.query(sql, employeeName, (err) => {
+                            if (err) throw err;
+                            console.log("Employee has been added!")
+                            viewAllEmployees();
+                        });
+                    });
+                });
+            });
+        });
+    });
+};
+
+// -------------- Update Employee --------------
+
+// -------------- Delete Employee --------------
+
+// 
+
     {
         type: "input",
         name: "add-employee",
